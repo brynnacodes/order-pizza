e3e5cdef6ef4472bcfe95baa280d95427a00f0a1
Brynna McCarter <brynna.mccarter@gmail.com>
2017-01-27 10:13:10 -0800

    add functionality to determine price based on toppings and one size

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 //back-end
 
+var toppings = [];
 
-function Pizza (toppings) {
+function Pizza (toppings, size) {
   this.toppings = toppings;
+  this.size = size;
+};
+
+Pizza.prototype.pizzaPrice = function () {
+  var cost = 10;
+
+  for (i = 1; i <= toppings.length; i++) {
+    cost +=1;
+  }
+  return cost;
 };
 
 //front-end
@@ -12,12 +23,19 @@ $(document).ready(function() {
     event.preventDefault();
 
     $("input:checkbox[name=pizza-toppings]:checked").each(function(){
-      var pizzaToppings = $(this).val();
-
-    var pizzaSize = $("select#pizza-size").val();
-
-    $("#pizza-order").append("<p> Your order: " + pizzaSize + " " + pizzaToppings + " pizza</p>");
-
+      toppings.push($(this).val());
     });
+
+    console.log(toppings);
+
+    size = $("select#pizza-size").val();
+
+      var newPizza = new Pizza(toppings, size);
+      var cost = 10;
+      var pizzaTotal = newPizza.pizzaPrice();
+
+    $("#pizza-order").append("<p> Your order: " + size + " " + toppings + ", " + "price:" + " " + "$" + pizzaTotal + "</p>");
+
+
   });
 });
